0d739a6af516c816d9f670839fefa2b26ed787b7
viaya-ya <79787512027@mail.ru>
2023-07-05 13:54:39 +0300

    fix application configs in order to run on windows

--- src/main/java/com/example/projects/controllers/MainController.java
+++ src/main/java/com/example/projects/controllers/MainController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping
+    @GetMapping("/")
     public String index() {
-        return "index";
+        return "index.html";
     }
 }
