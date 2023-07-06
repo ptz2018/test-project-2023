7d338cb460eb9e78a90f3f12c98142e83f0367b2
user4 <ilya55vax@gmail.com>
2023-07-06 16:04:11 +0300

    Removed unused imports.

--- src/main/java/com/example/projects/configuration/WebConfig.java
+++ src/main/java/com/example/projects/configuration/WebConfig.java
@@ -3,7 +3,6 @@ package com.example.projects.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 public class WebConfig {
--- src/main/java/com/example/projects/model/Group.java
+++ src/main/java/com/example/projects/model/Group.java
@@ -1,7 +1,5 @@
 package com.example.projects.model;
 
-import org.hibernate.annotations.Type;
-
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
